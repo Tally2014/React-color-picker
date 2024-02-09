8b3941270078ead8a91b4b55085ea83ec2d18663
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-09 13:28:35 +0200

    ColorPicker HTML Markup

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,7 +1,15 @@
 import React, {useState} from 'react';
 
 export default function ColorPicker() {
+    const [color, setColor] = useState('#ffffff');
   return (
-    <div>ColorPicker</div>
+    <div className='color-picker-container'>
+        <h1>Color Picker</h1>
+        <div className='color-display' style={{backgroundColor: color}}>
+            <p>Selected Color: {color}</p>
+        </div>
+        <label>Select a Color:</label>
+        <input type="color" value={color}></input>
+    </div>
   )
 }
